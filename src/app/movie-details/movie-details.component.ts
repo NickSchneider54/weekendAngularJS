839373ec0dae58608ec44a43f0c4bb41839acf82
Nick <ns0913569@otc.edu>
2019-11-17 12:17:59 -0600

    Duct-taped Trailers up and running

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { MoviesService } from '../movies.service';
 import { Movie } from '../movie';
 import { ActivatedRoute } from '@angular/router';
+import * as moment from 'moment';
+import { DomSanitizer } from '@angular/platform-browser';
 
 
 @Component({
@@ -12,14 +14,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  public movieId;
-  public headerImg: string;
-  public movie: object = [];
-  public reviews = [];
+  movieId;
+  headerImg: string;
+  releaseDate: string;
+  movie: object = [];
+  reviews = [];
+  trailers = [];
+  officialTrailer: string;
+  videoUrl;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) { }
-
-
+  constructor(private http: HttpClient, private route: ActivatedRoute, public sanitizer: DomSanitizer) { }
+ 
 
   ngOnInit() {
     let id  = parseInt(this.route.snapshot.paramMap.get('id'));
@@ -29,17 +34,30 @@ export class MovieDetailsComponent implements OnInit {
       .subscribe((result: any = []) =>{        
         this.movie = new Movie(this.movieId, result.backdrop_path, result.title, result.release_date, result.overview, result.vote_average);
         this.headerImg = `https://image.tmdb.org/t/p/original${result.backdrop_path}`;
+        this.releaseDate = moment(result.release_date, 'YYYY-MM-DD').format("MMM DD, YYYY");
         console.log(this.headerImg)
         console.log(this.movie)
+    });
+
+    this.http.get(`https://api.themoviedb.org/3/movie/${this.movieId}/videos?api_key=88fa8cb9c6ebb34aaa7cc7e7e074c1a9&language=en-US`)
+      .subscribe((result: any = []) =>{
+        for(var i = 0; i < result.results.length; i++){
+          this.trailers[i] = result.results[i];
+        }
+        this.officialTrailer = result.results[0].key;
+        this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.officialTrailer}`)
+        console.log(this.officialTrailer)
       });
 
     this.http.get(`https://api.themoviedb.org/3/movie/${this.movieId}/reviews?api_key=88fa8cb9c6ebb34aaa7cc7e7e074c1a9&language=en-US&page=1`)
       .subscribe((result: any = []) =>{
-        for(var i = 0; i <result.results.length; i++){
+        for(var i = 0; i < result.results.length; i++){
           this.reviews[i] = result.results[i];
         }
-      });     
+    });     
     
+    
+
   }
 
   bgImg(){
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Movie } from '../movie';
 import { MoviesService } from '../movies.service';
 import { Router } from '@angular/router'
+import * as moment from 'moment';
 
 @Component({
   selector: 'movie-list',
@@ -18,7 +18,8 @@ export class MovieListComponent implements OnInit {
   ngOnInit() {
     this.movieService.getMovies().subscribe((result: any = [])=>{
       for(var i = 0; i < result.results.length; i++){
-        this.movies.push(new Movie(result.results[i].id, result.results[i].poster_path, result.results[i].title, result.results[i].release_date, result.results[i].overview, result.results[i].vote_average))
+        var releaseDate = moment(result.results[i].release_date, 'YYYY-MM-DD').format("MM-DD-YYYY");
+        this.movies.push(new Movie(result.results[i].id, result.results[i].poster_path, result.results[i].title, releaseDate, result.results[i].overview, result.results[i].vote_average))
       }
       console.log(this.movies);
     })
