86755cd8e6f7ba287bd45c47203e5c80a68e416d
Nick <ns0913569@otc.edu>
2019-11-19 11:44:14 -0600

    added pagination to movies

--- src/app/Movies/movie-list/movie-list.component.ts
+++ src/app/Movies/movie-list/movie-list.component.ts
@@ -3,6 +3,7 @@ import { Movie } from '../movie';
 import { MoviesService } from '../movies.service';
 import { Router } from '@angular/router'
 import * as moment from 'moment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'movie-list',
@@ -12,17 +13,30 @@ import * as moment from 'moment';
 export class MovieListComponent implements OnInit {
 
   movies: Movie[] = [];
+  startPoint = 0;
+  endPoint = 21;
 
-  constructor(private movieService: MoviesService, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
-    this.movieService.getMovies().subscribe((result: any = [])=>{
-      for(var i = 0; i < result.results.length; i++){
-        var releaseDate = moment(result.results[i].release_date, 'YYYY-MM-DD').format("MM-DD-YYYY");
-        this.movies.push(new Movie(result.results[i].id, result.results[i].poster_path, result.results[i].title, releaseDate, result.results[i].overview, result.results[i].vote_average))
-      }
-      console.log(this.movies);
-    })
+    for(var i = 1; i <= 10; i++){
+      this.http.get(`https://api.themoviedb.org/3/movie/popular?api_key=88fa8cb9c6ebb34aaa7cc7e7e074c1a9&language=en-US&page=${i}`).subscribe((result: any = [])=>{
+        for(var i = 0; i < result.results.length; i++){
+          var releaseDate = moment(result.results[i].release_date, 'YYYY-MM-DD').format("MM-DD-YYYY");
+          this.movies.push(new Movie(result.results[i].id, result.results[i].poster_path, result.results[i].title, releaseDate, result.results[i].overview, result.results[i].vote_average))
+        }
+        console.log(this.movies);
+      })
+    }
+  }
+
+  getArrayLength(length){
+    return new Array(length/20);
+  }
+
+  updatePageIndex(pageIndex){
+    this.startPoint = pageIndex * 21;
+    this.endPoint = this.startPoint + 21;
   }
 
   onSelect(movie){
